c1330c893c13e86c47c0b317a3f249c2c8ee394c
jeonghoon <shb03323@naver.com>
2023-09-02 17:28:57 +0900

    feat: 플레이어 상태 입력 기능 구현

--- src/main/java/bossmonster/view/InputView.java
+++ src/main/java/bossmonster/view/InputView.java
@@ -1,9 +1,13 @@
 package bossmonster.view;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+
+    private static final String PLAYER_STATUS_SPLITTER = ",";
 
     private static final Scanner sc = new Scanner(System.in);
 
@@ -22,5 +26,42 @@ public class InputView {
     public String inputPlayerName() {
         System.out.println("플레이어의 이름을 입력해주세요.");
         return sc.nextLine();
+    }
+
+    public List<Integer> inputPlayerStatus() {
+        System.out.println("플레이어의 HP와 MP를 입력해주세요.(,로 구분)");
+        final List<String> inputs = splitPlayerStatusInput(sc.nextLine());
+        return mapToNumber(inputs);
+    }
+
+    private List<String> splitPlayerStatusInput(final String input) {
+        final List<String> splitInput = Arrays.stream(input.split(PLAYER_STATUS_SPLITTER)).toList();
+        validateInputCount(splitInput);
+        for (String eachInput : splitInput) {
+            validateInputNotBlank(eachInput);
+        }
+        return splitInput;
+    }
+
+    private void validateInputCount(final List<String> splitInput) {
+        if (splitInput.size() != 2) {
+            throw new IllegalArgumentException("HP와 MP 두 가지만 입력해주세요. 구분자는 , 입니다.");
+        }
+    }
+
+    private void validateInputNotBlank(final String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("HP와 MP에 공백을 입력하면 안됩니다.");
+        }
+    }
+
+    private List<Integer> mapToNumber(final List<String> inputs) {
+        try {
+            return inputs.stream()
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("HP와 MP는 숫자이어야 합니다.");
+        }
     }
 }
