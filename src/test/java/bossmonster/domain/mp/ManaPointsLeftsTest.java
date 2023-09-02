d5fb43554a6bb41b4609cb700b00bca1fba6fd09
jeonghoon <shb03323@naver.com>
2023-09-02 19:25:23 +0900

    test: 마나 소모 시 0보다 작게 되면 예외가 발생하는 테스트 작성

--- src/test/java/bossmonster/domain/mp/ManaPointsLeftsTest.java
+++ src/test/java/bossmonster/domain/mp/ManaPointsLeftsTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ManaPointsLeftsTest {
 
-    @DisplayName("남은 마나를 첫 생성시에는 초기 마나와 동일한 마나로 생성된다.")
+    @DisplayName("남은 MP를 첫 생성시에는 초기 마나와 동일한 MP로 생성된다.")
     @Test
     void init_value_is_same_as_InitialManaPoints() {
         // given
@@ -19,5 +20,18 @@ class ManaPointsLeftsTest {
 
         // then
         assertThat(initialManaPointsLefts.getValue()).isEqualTo(initialManaPoints.getValue());
+    }
+
+    @DisplayName("MP를 소모할 때 MP가 0 미만이 되면 예외가 발생한다.")
+    @Test
+    void throw_exception_when_mana_changes_under_0() {
+        // given
+        final int initialValue = 10;
+        final int manaChanges = -20;
+        final ManaPointsLefts initialManaPointsLefts = ManaPointsLefts.init(new InitialManaPoints(initialValue));
+
+        // when, then
+        assertThatThrownBy(() -> initialManaPointsLefts.changeManaPoints(manaChanges))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
