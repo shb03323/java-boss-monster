3102ff08b2eab845583e195c0374f22f247d304f
jeonghoon <shb03323@naver.com>
2023-09-02 17:36:03 +0900

    feat: 플레이어 생성 기능 구현

--- src/main/java/bossmonster/controller/BossGameController.java
+++ src/main/java/bossmonster/controller/BossGameController.java
@@ -1,8 +1,12 @@
 package bossmonster.controller;
 
 import bossmonster.domain.Boss;
+import bossmonster.domain.Player;
 import bossmonster.view.InputView;
 import bossmonster.view.OutputView;
+import bossmonster.view.dto.PlayerStatusInputDto;
+
+import java.util.List;
 
 public class BossGameController {
 
@@ -16,6 +20,7 @@ public class BossGameController {
 
     public void runGame() {
         final Boss boss = initBoss();
+        final Player player = initPlayer();
     }
 
     private Boss initBoss() {
@@ -24,6 +29,17 @@ public class BossGameController {
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return initBoss();
+        }
+    }
+
+    private Player initPlayer() {
+        try {
+            final String playerName = inputView.inputPlayerName();
+            final PlayerStatusInputDto playerStatusInputDto = inputView.inputPlayerStatus();
+            return Player.of(playerName, playerStatusInputDto.getInputHP(), playerStatusInputDto.getInputMP());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return initPlayer();
         }
     }
 }
