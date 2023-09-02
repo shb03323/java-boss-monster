7bcbeb54ddee803e517653b69729dbf4924530bb
jeonghoon <shb03323@naver.com>
2023-09-02 17:11:45 +0900

    feat: 플레이어 생성 기능 구현

--- src/main/java/bossmonster/domain/Player.java
+++ src/main/java/bossmonster/domain/Player.java
@@ -5,8 +5,14 @@ public class Player {
     private final PlayerName playerName;
     private final PlayerStatus playerStatus;
 
-    public Player(final PlayerName playerName, final PlayerStatus playerStatus) {
+    private Player(final PlayerName playerName, final PlayerStatus playerStatus) {
         this.playerName = playerName;
         this.playerStatus = playerStatus;
+    }
+
+    public static Player of(final String inputName, final int inputHP, final int inputMP) {
+        final PlayerName playerName = new PlayerName(inputName);
+        final PlayerStatus playerStatus = PlayerStatus.of(inputHP, inputMP);
+        return new Player(playerName, playerStatus);
     }
 }
